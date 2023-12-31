70c0b582a34623f9690c916febefd41d7cbf41d9
liyukun <liyukun@cryptape.com>
2023-12-31 22:56:42 +0800

    bug: add more connected code_hash for compatible intense

--- contracts/cluster_proxy/build.rs
+++ contracts/cluster_proxy/build.rs
@@ -1,15 +1,37 @@
 use ckb_hash::blake2b_256;
 use std::{env, fs};
 
+pub fn concat_code_hashes(var_name: &str, code_hashes: &[[u8; 32]]) -> String {
+    let mut content = format!(
+        "pub const {var_name}: [[u8; 32]; {}] = [",
+        code_hashes.len()
+    );
+    code_hashes.into_iter().for_each(|v| {
+        content += &format!("{v:?},");
+    });
+    content += "];\n";
+    content
+}
+
 pub fn main() {
     let compile_mode = env::var("PROFILE").unwrap();
     let cluster_path = env::current_dir()
         .unwrap()
         .join("../../build")
-        .join(compile_mode)
+        .join(&compile_mode)
         .join("cluster");
     let cluster = std::fs::read(cluster_path).expect("load cluster");
     let code_hash = blake2b_256(cluster);
-    let file = format!("pub const CLUSTER_CODE_HASHES: [[u8; 32]; 1] = [{code_hash:?}];\n");
-    fs::write("./src/hash.rs", file).unwrap();
+
+    let mut cluster_code_hashes = vec![code_hash];
+    // this is version v1 of cluster contract in testnet
+    cluster_code_hashes.push(
+        hex::decode("598d793defef36e2eeba54a9b45130e4ca92822e1d193671f490950c3b856080")
+            .unwrap()
+            .try_into()
+            .unwrap(),
+    );
+
+    let content = concat_code_hashes("CLUSTER_CODE_HASHES", &cluster_code_hashes);
+    fs::write("./src/hash.rs", content).unwrap();
 }
--- contracts/spore/build.rs
+++ contracts/spore/build.rs
@@ -1,8 +1,7 @@
 use ckb_hash::blake2b_256;
 use std::{env, fs};
 
-fn load_code_hash(binary_name: &str) -> [u8; 32] {
-    let compile_mode = env::var("PROFILE").unwrap();
+fn load_code_hash(binary_name: &str, compile_mode: &str) -> [u8; 32] {
     let binary_path = env::current_dir()
         .unwrap()
         .join("../../build")
@@ -12,10 +11,33 @@ fn load_code_hash(binary_name: &str) -> [u8; 32] {
     blake2b_256(binary)
 }
 
-pub fn main() {
-    let cluster_code_hash = load_code_hash("cluster");
-    let cluster_agent_code_hash = load_code_hash("cluster_agent");
+pub fn concat_code_hashes(var_name: &str, code_hashes: &[[u8; 32]]) -> String {
+    let mut content = format!(
+        "pub const {var_name}: [[u8; 32]; {}] = [",
+        code_hashes.len()
+    );
+    code_hashes.into_iter().for_each(|v| {
+        content += &format!("{v:?},");
+    });
+    content += "];\n";
+    content
+}
 
-    let file = format!("pub const CLUSTER_CODE_HASHES: [[u8; 32]; 1] = [{cluster_code_hash:?}];\npub const CLUSTER_AGENT_CODE_HASHES: [[u8; 32]; 1] = [{cluster_agent_code_hash:?}];\n");
-    fs::write("./src/hash.rs", file).unwrap();
+pub fn main() {
+    let compile_mode = env::var("PROFILE").unwrap();
+    let cluster_code_hash = load_code_hash("cluster", &compile_mode);
+    let cluster_agent_code_hash = load_code_hash("cluster_agent", &compile_mode);
+
+    let mut cluster_code_hashes = vec![cluster_code_hash];
+    // this is version v1 of cluster contract in testnet
+    cluster_code_hashes.push(
+        hex::decode("598d793defef36e2eeba54a9b45130e4ca92822e1d193671f490950c3b856080")
+            .unwrap()
+            .try_into()
+            .unwrap(),
+    );
+
+    let mut content = concat_code_hashes("CLUSTER_CODE_HASHES", &cluster_code_hashes);
+    content += concat_code_hashes("CLUSTER_AGENT_CODE_HASHES", &[cluster_agent_code_hash]).as_str();
+    fs::write("./src/hash.rs", content).unwrap();
 }
