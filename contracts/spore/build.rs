52c1689611e9e7b242dc80c387774a3f09e5bcbd
liyukun <liyukun@cryptape.com>
2024-01-05 14:23:54 +0800

    feat: add frozen code hashes management

--- contracts/spore/build.rs
+++ contracts/spore/build.rs
@@ -1,6 +1,8 @@
 use ckb_hash::blake2b_256;
 use std::{env, fs};
 
+use spore_build_tools::{concat_code_hashes, load_frozen_toml};
+
 fn load_code_hash(binary_name: &str, compile_mode: &str) -> [u8; 32] {
     let binary_path = env::current_dir()
         .unwrap()
@@ -11,36 +13,20 @@ fn load_code_hash(binary_name: &str, compile_mode: &str) -> [u8; 32] {
     blake2b_256(binary)
 }
 
-pub fn concat_code_hashes(var_name: &str, code_hashes: &[[u8; 32]]) -> String {
-    let mut content = format!(
-        "pub const {var_name}: [[u8; 32]; {}] = [",
-        code_hashes.len()
-    );
-    code_hashes.into_iter().for_each(|v| {
-        content += &format!("{v:?},");
-    });
-    content += "];\n";
-    content
-}
-
 pub fn main() {
     let compile_mode = env::var("PROFILE").unwrap();
     let cluster_code_hash = load_code_hash("cluster", &compile_mode);
     let cluster_agent_code_hash = load_code_hash("cluster_agent", &compile_mode);
 
-    let mut cluster_code_hashes = vec![];
-    if let Err(env::VarError::NotPresent) = env::var("CARGO_FEATURE_RELEASE_EXPORT") {
-        // this is version v1 of cluster contract in testnet
-        cluster_code_hashes.push(
-            hex::decode("598d793defef36e2eeba54a9b45130e4ca92822e1d193671f490950c3b856080")
-                .unwrap()
-                .try_into()
-                .unwrap(),
-        );
-    }
-    cluster_code_hashes.push(cluster_code_hash);
+    let frozen = load_frozen_toml();
+    let cluster_code_hashes = [frozen.cluster_code_hashes(), vec![cluster_code_hash]].concat();
+    let cluster_agent_code_hashes = [
+        frozen.cluster_agent_code_hashes(),
+        vec![cluster_agent_code_hash],
+    ]
+    .concat();
 
     let mut content = concat_code_hashes("CLUSTER_CODE_HASHES", &cluster_code_hashes);
-    content += concat_code_hashes("CLUSTER_AGENT_CODE_HASHES", &[cluster_agent_code_hash]).as_str();
+    content += concat_code_hashes("CLUSTER_AGENT_CODE_HASHES", &cluster_agent_code_hashes).as_str();
     fs::write("./src/hash.rs", content).unwrap();
 }
---
+++ lib/build/src/lib.rs
@@ -0,0 +1,91 @@
+use faster_hex::hex_decode;
+use std::env;
+use std::fs;
+
+#[derive(serde::Deserialize)]
+struct CodeHashList {
+    code_hash_list: Vec<String>,
+}
+
+#[derive(serde::Deserialize)]
+pub struct PublishedCodeHash {
+    spore: CodeHashList,
+    cluster: CodeHashList,
+    cluster_proxy: CodeHashList,
+    cluster_agent: CodeHashList,
+    mutant: CodeHashList,
+}
+
+fn hex_to_byte32(hex: &str) -> [u8; 32] {
+    let mut byte32 = [0u8; 32];
+    hex_decode(hex.as_bytes(), &mut byte32).expect("hex to byte32");
+    byte32
+}
+
+impl PublishedCodeHash {
+    pub fn spore_code_hashes(&self) -> Vec<[u8; 32]> {
+        self.spore
+            .code_hash_list
+            .iter()
+            .map(|v| hex_to_byte32(v))
+            .collect()
+    }
+
+    pub fn cluster_code_hashes(&self) -> Vec<[u8; 32]> {
+        self.cluster
+            .code_hash_list
+            .iter()
+            .map(|v| hex_to_byte32(v))
+            .collect()
+    }
+
+    pub fn cluster_proxy_code_hashes(&self) -> Vec<[u8; 32]> {
+        self.cluster_proxy
+            .code_hash_list
+            .iter()
+            .map(|v| hex_to_byte32(v))
+            .collect()
+    }
+
+    pub fn cluster_agent_code_hashes(&self) -> Vec<[u8; 32]> {
+        self.cluster_agent
+            .code_hash_list
+            .iter()
+            .map(|v| hex_to_byte32(v))
+            .collect()
+    }
+
+    pub fn mutant_code_hashes(&self) -> Vec<[u8; 32]> {
+        self.mutant
+            .code_hash_list
+            .iter()
+            .map(|v| hex_to_byte32(v))
+            .collect()
+    }
+}
+
+pub fn load_frozen_toml() -> PublishedCodeHash {
+    let net_type = if env::var("CARGO_FEATURE_RELEASE_EXPORT").is_ok() {
+        "mainnet"
+    } else {
+        "testnet"
+    };
+    let frozen_path = env::current_dir()
+        .unwrap()
+        .join("../../deployment/frozen")
+        .join(format!("{net_type}.toml"));
+    let frozen = fs::read_to_string(frozen_path).unwrap();
+    toml::from_str(&frozen).unwrap()
+}
+
+pub fn concat_code_hashes(var_name: &str, code_hashes: &[[u8; 32]]) -> String {
+    let mut content = format!(
+        "pub const {var_name}: [[u8; 32]; {}] = [",
+        code_hashes.len()
+    );
+    code_hashes.into_iter().for_each(|v| {
+        content += &format!("{v:?},");
+    });
+    content += "];\n";
+    content
+}
